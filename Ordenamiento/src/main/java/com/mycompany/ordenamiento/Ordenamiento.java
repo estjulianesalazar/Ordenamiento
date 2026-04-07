52e97c67091af6f7a24c1226b1e1d39fd4a8d368
Julian <est.juliane.salazar@unimilitar.edu.co>
2026-04-07 10:33:24 -0500

    Se puso en comentarios el metodo de ordenamiento tipo burbuja y se implemento un metodo de ordenamiento de tipo seleccion en el cual se mira todo el arreglo para ir poniendo el dato minimo de posicion actual.

--- Ordenamiento/src/main/java/com/mycompany/ordenamiento/Ordenamiento.java
+++ Ordenamiento/src/main/java/com/mycompany/ordenamiento/Ordenamiento.java
@@ -31,14 +31,29 @@ public class Ordenamiento {
         
     }  
     private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length-1; i++){
-            for (int j = i+1; j < v.length ; j++){
-                if(v[i] > v[j]){
-                    int w = v[j];
-                    v[j] = v[i];
-                    v[i] = w;
+        //for (int i = 0; i < v.length-1; i++){
+          //  for (int j = i+1; j < v.length ; j++){
+             //   if(v[i] > v[j]){
+                //    int w = v[j];
+                //    v[j] = v[i];
+                //    v[i] = w;
+               // }
+            //}
+        //}
+    //}
+//}
+        for (int i = 0; i < v.length -1; i++){
+            int minIndex = i;
+            
+            for (int j = i + 1; j < v.length; j++){
+                if(v[j] < v[minIndex]){
+                    minIndex = j;
                 }
             }
+            
+            int temp = v[i];
+            v[i] = v[minIndex];
+            v[minIndex]= temp;
         }
     }
 }
